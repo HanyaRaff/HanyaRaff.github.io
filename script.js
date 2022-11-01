51ddc3b57ddf93bc164b1cd07025274562a66abd
HanyaRaff <rafflymaulana5@gmail.com>
2022-11-01 21:33:11 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function home() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function about() {
+    document.body.scrollTop = 450;
+    document.documentElement.scrollTop = 450;
+}
+function skills() {
+    document.body.scrollTop = 918;
+    document.documentElement.scrollTop = 918;
+}
+function project() {
+    document.body.scrollTop = 1318;
+    document.documentElement.scrollTop = 1318;
+}
